c208d8059f8528bd2a55e3ceb155f8720f4efec9
Diego Bernal <diego91bernal@gmail.com>
2024-01-25 17:51:42 -0800

    remove dimensions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,8 +16,8 @@ export default function Home() {
         <meta property="og:video:secure_url" content="https://content.niftyisland.com/images/6678b109-397e-40d6-9591-325a21addec8.mp4"/>
         {/* <meta property="og:video:type" content="video/mp4" /> */}
         <meta property="og:type" content="video"/>
-        <meta property="og:video:width" content="1000px" />
-        <meta property="og:video:height" content="1000px" />
+        {/* <meta property="og:video:width" content="1000px" />
+        <meta property="og:video:height" content="1000px" /> */}
       </Head>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
