48a016c1b1ff41ea3f7b0f980f0ee4323a5ab461
Diego Bernal <diego91bernal@gmail.com>
2024-01-29 13:51:42 -0800

    Add default chromium executable path

--- src/pages/api/generate-og.tsx
+++ src/pages/api/generate-og.tsx
@@ -1,9 +1,23 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 import puppeteer, {Browser} from 'puppeteer-core'
 import {renderToStaticMarkup} from 'react-dom/server'
+import os from 'os'
 import chromium from 'chrome-aws-lambda'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
+
+let DEFAULT_EXECUTABLE_PATH: string | null = null
+  if (process.platform === 'win32') {
+    if (['arm64', 'ppc64', 'x64', 's390x'].includes(os.arch())) {
+      DEFAULT_EXECUTABLE_PATH = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    } else {
+      DEFAULT_EXECUTABLE_PATH = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    }
+  } else if (process.platform === 'linux') {
+    DEFAULT_EXECUTABLE_PATH = '/usr/bin/google-chrome'
+  } else {
+    DEFAULT_EXECUTABLE_PATH = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  }
 
 export default async function handler(
   req: NextApiRequest,
@@ -22,14 +36,16 @@ export default async function handler(
   }
 
   console.log("CHROMIUM", await chromium.executablePath)
+  console.log("DEFAULT_EXECUTABLE_PATH", DEFAULT_EXECUTABLE_PATH)
   try {
+    const executablePath = await chromium.executablePath ?? DEFAULT_EXECUTABLE_PATH
     browser = await puppeteer.launch({
       defaultViewport: {
         width: 500,
         height: 350,
       },
       headless: chromium.headless,
-      executablePath: await chromium.executablePath ?? undefined,
+      executablePath,
       args: chromium.args,
     })
 
