cf988db06072f044a2bc0e3f12cd59e1ebc8ebad
Diego Bernal <diego91bernal@gmail.com>
2024-02-06 15:18:32 -0800

    Mark puppeteer and chromium as external

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // experimental: {
-  //   serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
-  // },
+  experimental: {
+    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+  },
 }
 
 export default nextConfig
