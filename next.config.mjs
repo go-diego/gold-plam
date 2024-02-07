2839945aff8b5c51508ee258e547e938425e3303
Diego Bernal <diego91bernal@gmail.com>
2024-02-06 16:03:41 -0800

    Updatate config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    serverComponentsExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
+    serverComponentsExternalPackages: [
+      'puppeteer-core',
+      '@sparticuz/chromium-min',
+    ],
   },
 }
 
